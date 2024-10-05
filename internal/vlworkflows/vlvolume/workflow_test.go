5e2c67beab7c96decc314204ed9b64396f0acca2
Andy Kreling <krelinga@gmail.com>
2024-10-05 04:37:19 +0000

    Placeholder for a test of actual workflow run.

--- internal/vlworkflows/vlvolume/workflow_test.go
+++ internal/vlworkflows/vlvolume/workflow_test.go
@@ -22,6 +22,12 @@ func (s *WorkflowTestSuite) AfterTest(suiteName, testName string) {
 	s.env.AssertExpectations(s.T())
 }
 
+func (s *WorkflowTestSuite) TestWorkflow() {
+	s.env.ExecuteWorkflow(Workflow, nil)
+	s.True(s.env.IsWorkflowCompleted())
+	s.NoError(s.env.GetWorkflowError())
+}
+
 func TestWorkflowTestSuite(t *testing.T) {
 	t.Parallel()
 	suite.Run(t, new(WorkflowTestSuite))
