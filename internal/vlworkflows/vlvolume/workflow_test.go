01576ea20a12769d17ae1ea01348f399ba42dfc1
Andy Kreling <krelinga@gmail.com>
2024-10-05 04:27:10 +0000

    skeleton workflow test suite for volume actor.

---
+++ internal/vlworkflows/vlvolume/workflow_test.go
@@ -0,0 +1,28 @@
+package vlvolume
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/suite"
+	"go.temporal.io/sdk/testsuite"
+)
+
+type WorkflowTestSuite struct {
+	suite.Suite
+	testsuite.WorkflowTestSuite
+
+	env *testsuite.TestWorkflowEnvironment
+}
+
+func (s *WorkflowTestSuite) SetupTest() {
+	s.env = s.NewTestWorkflowEnvironment()
+}
+
+func (s *WorkflowTestSuite) AfterTest(suiteName, testName string) {
+	s.env.AssertExpectations(s.T())
+}
+
+func TestWorkflowTestSuite(t *testing.T) {
+	t.Parallel()
+	suite.Run(t, new(WorkflowTestSuite))
+}
