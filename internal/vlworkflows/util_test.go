fb86047e6453bfee6e7e5104307844e68d6c1ada
Andy Kreling <krelinga@gmail.com>
2024-10-05 14:45:00 +0000

    generalize and simplify handling of assertions in update callbacks.

--- internal/vlworkflows/util_test.go
+++ internal/vlworkflows/util_test.go
@@ -19,3 +19,36 @@ func assertContinuedWithState[StateType any](a *assert.Assertions, err error, ex
 	conv.FromPayloads(cont.Input, &actualState)
 	return a.Equal(expectedState, actualState)
 }
+
+type updateCallbacks[outType any] struct {
+	accept func()
+	reject func(error)
+	complete func(*outType, error)
+}
+
+func (cb *updateCallbacks[outType]) Accept() {
+	if cb.accept != nil {
+		cb.accept()
+	}
+}
+
+func (cb *updateCallbacks[outType]) Reject(err error) {
+	if cb.reject != nil {
+		cb.reject(err)
+	}
+}
+
+func (cb *updateCallbacks[outType]) Complete(success any, err error) {
+	if cb.complete != nil {
+		cb.complete(success.(*outType), err)
+	}
+}
+
+func assertComplete[outType any](a *assert.Assertions, out *outType, err error) *updateCallbacks[outType] {
+	return &updateCallbacks[outType]{
+		complete: func(actual *outType, actualErr error) {
+			a.Equal(out, actual)
+			a.ErrorIs(err, actualErr)
+		},
+	}
+}
--- internal/vlworkflows/volume_test.go
+++ internal/vlworkflows/volume_test.go
@@ -5,7 +5,6 @@ import (
 	"time"
 
 	"github.com/krelinga/video-library/internal/vlactivities"
-	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/suite"
 	"go.temporal.io/sdk/client"
@@ -38,29 +37,6 @@ func (s *VolumeTestSuite) TestFreshlyCreated() {
 	assertContinuedWithState(s.Assertions, err, &VolumeState{})
 }
 
-type newDiscsUpdateCBs struct {
-	a        *assert.Assertions
-	expected []string
-}
-
-func (cb *newDiscsUpdateCBs) Accept() {}
-
-func (cb *newDiscsUpdateCBs) Reject(err error) {
-	cb.a.NoError(err)
-}
-
-func (cb *newDiscsUpdateCBs) Complete(success any, err error) {
-	if !cb.a.NoError(err) {
-		return
-	}
-	switch s := success.(type) {
-	case *VolumeDiscoverNewDiscsUpdateResponse:
-		cb.a.ElementsMatch(cb.expected, s.Discovered)
-	default:
-		cb.a.Fail("unexpected type", success)
-	}
-}
-
 func (s *VolumeTestSuite) TestDiscoverNewDiscs() {
 	s.env.RegisterWorkflow(Disc)
 	s.env.OnActivity(vlactivities.VolumeReadDiscNames, mock.Anything, "test_volume").Return([]string{"disc1", "disc2"}, nil)
@@ -73,7 +49,9 @@ func (s *VolumeTestSuite) TestDiscoverNewDiscs() {
 	s.env.RegisterDelayedCallback(
 		func() {
 			s.env.UpdateWorkflowByID("test_volume", VolumeDiscoverNewDiscsUpdate, "",
-				&newDiscsUpdateCBs{a: s.Assertions, expected: []string{"test_volume/disc2"}}, nil)
+				assertComplete(s.Assertions, &VolumeDiscoverNewDiscsUpdateResponse{
+					Discovered: []string{"test_volume/disc2"},
+				}, nil), nil)
 		}, time.Hour)
 	s.env.ExecuteWorkflow(Volume, state)
 	s.True(s.env.IsWorkflowCompleted())
