ca02c4ff6dc5303923bac29c9151c6282526687a
Andy Kreling <krelinga@gmail.com>
2024-10-07 06:10:32 +0000

    tests for NewDiscWfId().

--- internal/vltemp/disc_test.go
+++ internal/vltemp/disc_test.go
@@ -2,6 +2,7 @@ package vltemp
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	"github.com/krelinga/video-library/internal/vlconfig"
@@ -13,4 +14,49 @@ func TestDiscPath(t *testing.T) {
 	ctx := vlcontext.WithConfig(context.Background(), &vlconfig.Root{Volume: &vlconfig.Volume{Directory: "/mocked/path"}})
 	gotPath := DiscPath(ctx, DiscWfId("volume1/disc1"))
 	assert.Equal(t, "/mocked/path/volume1/disc1", gotPath)
+}
+func TestNewDiscWfId(t *testing.T) {
+	tests := []struct {
+		volumeWfId   VolumeWfId
+		discFilename string
+		expectedId   DiscWfId
+		expectError  bool
+	}{
+		{
+			volumeWfId:   VolumeWfId("volume1"),
+			discFilename: "disc1",
+			expectedId:   DiscWfId("volume1/disc1"),
+			expectError:  false,
+		},
+		{
+			volumeWfId:   VolumeWfId("volume2"),
+			discFilename: "disc2",
+			expectedId:   DiscWfId("volume2/disc2"),
+			expectError:  false,
+		},
+		{
+			volumeWfId:   VolumeWfId("volume3"),
+			discFilename: "",
+			expectError:  true,
+		},
+		{
+			volumeWfId:   "",
+			discFilename: "disc4",
+			expectError:  true,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(fmt.Sprintf("%s/%s", tt.volumeWfId, tt.discFilename), func(t *testing.T) {
+			id, err := NewDiscWfId(tt.volumeWfId, tt.discFilename)
+			if tt.expectError {
+				assert.ErrorIs(t, err, ErrInvalidWorkflowId)
+				assert.Panics(t, func() { id.Name() })
+				assert.Panics(t, func() { id.VolumeWfId() })
+			} else {
+				assert.NoError(t, err)
+				assert.Equal(t, tt.expectedId, id)
+			}
+		})
+	}
 }
