d9172e2cc5f2a10426ce9061cc63facea03cf263
Andy Kreling <krelinga@gmail.com>
2024-10-07 05:49:10 +0000

    simplify TestDiscPath() now that all error conditions are gone.

--- internal/vltemp/disc_test.go
+++ internal/vltemp/disc_test.go
@@ -10,30 +10,8 @@ import (
 )
 
 func TestDiscPath(t *testing.T) {
-	tests := []struct {
-		name     string
-		discWfId DiscWfId
-		wantPath string
-		wantErr  error
-	}{
-		{
-			name:     "valid discID",
-			discWfId: DiscWfId("volume1/disc1"),
-			wantPath: "/mocked/path/volume1/disc1",
-			wantErr:  nil,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			ctx := vlcontext.WithConfig(context.Background(), &vlconfig.Root{Volume: &vlconfig.Volume{Directory: "/mocked/path"}})
-			gotPath, err := DiscPath(ctx, tt.discWfId)
-			if tt.wantErr != nil {
-				assert.ErrorIs(t, err, tt.wantErr)
-			} else {
-				assert.NoError(t, err)
-				assert.Equal(t, tt.wantPath, gotPath)
-			}
-		})
-	}
+	ctx := vlcontext.WithConfig(context.Background(), &vlconfig.Root{Volume: &vlconfig.Volume{Directory: "/mocked/path"}})
+	gotPath, err := DiscPath(ctx, DiscWfId("volume1/disc1"))
+	assert.NoError(t, err)
+	assert.Equal(t, "/mocked/path/volume1/disc1", gotPath)
 }
